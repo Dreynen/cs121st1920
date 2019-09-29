026b783f0024c3263058a413e529dad7170407ca
Dreynen <jlhechanova@up.edu.ph>
2019-09-29 08:06:12 +0000

    did syntax changes and fixed engg method

--- recursion.java
+++ recursion.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class ArraySum {
     public static int sum(int i, int[] a) {
-        if (i == a.length) {
-            return 0;
-        }
+        if (i == a.length) return 0;
         return a[i] + sum(++i, a);
     }
 }
 
 class Tribonacci {
     public static int trib(int n) {
-        if (n == 0) { return 0; }
-        if (n == 1 || n == 2) { return 1; }
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
 
         return trib(n-1) + trib(n-2) + trib(n-3);
     }
@@ -51,14 +49,14 @@ class DoubleFx {
 
 class Reverse {
     public static String reverse(String s) {
-        if (s.length() == 1) { return s; }
+        if (s.length() == 1) return s;
         return reverse(s.substring(1)) + s.charAt(0);
     }
 }
 
 class GCF {
     public static int gcf(int x, int y) {
-        if (y == 0) { return x; }
+        if (y == 0) return x;
         return gcf(y, x % y);
     }
 }
@@ -80,23 +78,51 @@ class FlashDrive {
 }
 
 class Engg {
-    public static boolean engg(String s) {
-        if (s.equals("GG") && s.length() == 2) { return true; }
+    private static boolean first = true;
 
-        if (((s.charAt(0) == 'E' || s.charAt(0) == 'e') && s.charAt(1) != 'N')
-        || (s.substring(0,2).equals("GG") && (s.charAt(2) != 'E' || s.charAt(2) != 'e'))
-        || ((s.charAt(1) == 'E' || s.charAt(1) == 'e') && s.charAt(0) != 'N')) {
+	public static boolean engg(String s) {
+        if (s.equals("GG") && !first) {
+        	first = true;
+        	return true;
+        }
+
+        if (s.length() <= 2
+        || (first && s.charAt(0) != 'E')
+        || (s.charAt(0) == 'E' && s.charAt(1) != 'N')
+        || (s.charAt(1) == 'E' && s.charAt(0) != 'N')) {
+        	first = true;
             return false;
         }
 
+        if (first) first = false;
+
         return engg(s.substring(1));
     }
+
+	/*
+     A function that does the exact same as
+     Engg.engg(S), but in python and nicer.
+
+        def engg(S, first = True):
+
+            # Base case
+            if S == "GG" and not first:
+            	return True
+
+            if (len(S) <= 2
+            or (first and S[0] is not 'E')
+            or (S[0] is 'E' and S[1] is not 'N')
+            or (S[1] is 'E' and S[0] is not 'N')):
+                return False
+
+            return engg(S[1:], False)
+    */
 }
 
 class Reachable {
     public static boolean reachable(int sx, int sy, int dx, int dy) {
-        if (sx > dx || sy > dy) { return false; }
-        if (sx == dx && sy == dy) { return true; }
+        if (sx > dx || sy > dy) return false;
+        if (sx == dx && sy == dy) return true;
 
         return reachable(sx, sx + sy, dx, dy) || reachable(sx + sy, sy, dx, dy);
     }
@@ -105,6 +131,6 @@ class Reachable {
 class recursion {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Reachable.reachable(2,10,26,12));
+        System.out.println(Engg.engg(sc.next()));
     }
 }
